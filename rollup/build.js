4442cee6ef01e226e5b946fafd2b7df92b4bb630
haha58 <1461089222@qq.com>
2025-07-13 16:00:27 +0800

    -完善打包sdk

--- rollup/build.js
+++ rollup/build.js
@@ -1,13 +1,16 @@
 const path = require("path");
-const json = require("@rollup/plugin-json");
-const { babel } = require("@rollup/plugin-babel");
+const json = require("@rollup/plugin-json"); //用于将 JSON 文件作为模块导入
+const { babel } = require("@rollup/plugin-babel"); //用于使用 Babel 转译 JavaScript/TypeScript 代码
 const resolveFile = function (filePath) {
   return path.join(__dirname, filePath);
 };
+// 插件配置
 const plugins = [
+  //json 插件配置为 compact: true，表示输出的 JSON 会被压缩（去除空格和换行）。
   json({
     compact: true,
   }),
+  //babel 插件配置指定了处理的文件扩展名为 .js 和 .ts，babelHelpers 设置为 "bundled" 表示将 Babel 辅助函数内联到每个文件中，presets 使用了 @babel/env 并指定了目标浏览器。
   babel({
     extensions: [".js", ".ts"],
     babelHelpers: "bundled",
@@ -23,13 +26,14 @@ const plugins = [
     ],
   }),
 ];
+// 打包配置
 module.exports = [
   {
     plugins,
     input: resolveFile("../src/webEyeSDK.js"),
     output: {
       file: resolveFile("../dist/monitor.js"),
-      format: "iife",
+      format: "iife", //立即执行函数
       name: "monitor",
       sourcemap: true,
     },
@@ -39,7 +43,7 @@ module.exports = [
     input: resolveFile("../src/webEyeSDK.js"),
     output: {
       file: resolveFile("../dist/monitor.esm.js"),
-      format: "esm",
+      format: "esm", //es6模块化标准
       name: "monitor",
       sourcemap: true,
     },
@@ -49,7 +53,7 @@ module.exports = [
     input: resolveFile("../src/webEyeSDK.js"),
     output: {
       file: resolveFile("../dist/monitor.cjs.js"),
-      format: "cjs",
+      format: "cjs", //commonjs模块化标准
       name: "monitor",
       sourcemap: true,
     },
